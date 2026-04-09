593a2187e4d777e34710993e5bba70f393617291
Olzeev <olzeevmax@gmail.com>
2026-04-09 17:22:44 +0300

    Fix 401

--- kokossimo-backend/shop/apps.py
+++ kokossimo-backend/shop/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ShopConfig(AppConfig):
     name = 'shop'
+
+    def ready(self):
+        # Подключаем сигналы (например, инвалидация токенов при смене пароля).
+        from . import signals  # noqa: F401
